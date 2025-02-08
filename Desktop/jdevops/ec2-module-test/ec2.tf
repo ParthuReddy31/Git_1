d2dc54d99c84875b4e9f3e58c35fab4bbc35d4ae
parthureddy <parthuredy3@gmail.com>
2025-02-08 16:24:00 -0600

    added ec2-module-test directory

---
+++ Desktop/jdevops/ec2-module-test/ec2.tf
@@ -0,0 +1,8 @@
+module "ec2" {
+    source = "../terraform-aws-ec2"
+    sg_id = "sg-0b030fd5e73eac564"
+    # instance_type = "t3.small"
+}
+output "public_ip" {
+    value = module.ec2.public_ip
+}
